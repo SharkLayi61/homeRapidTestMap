7121a4101151291a2c0e289f2f19a39030ecaef3
SharkLayi61 <113686758+SharkLayi61@users.noreply.github.com>
2022-12-04 22:57:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 //設定地圖中心座標和縮放比例
 var map = L.map("map", {
   center: [22.7317117, 120.28759],
-  zoom: 12
+  zoom: 15
 });
-
+var lc = L.control.locate().addTo(map);
+lc.start();
 //載入 OpenStreetMap 地圖資訊
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var redIcon = new L.Icon({
-  iconUrl:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-//在中心座標，放上紅色定位圖標
-L.marker([22.7317117, 120.28759], { icon: redIcon }).addTo(map);
 
 var xhr = new XMLHttpRequest();
 xhr.open(
@@ -106,6 +94,7 @@ xhr.onload = function () {
         '">打開Google地圖</a>'
     );
 
+    //將圖標加入圖層
     markers.addLayer(mark);
   }
   map.addLayer(markers);
